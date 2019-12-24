6a82f8e97a538a8b89edf4d2925a9d548a066e72
Jacob Williams <jacobaw@gmail.com>
2019-12-24 10:12:29 -0800

    fix link

--- packages/blunt-instrument-demo/src/components/App/AppView.js
+++ packages/blunt-instrument-demo/src/components/App/AppView.js
@@ -6,7 +6,7 @@ function AppView() {
   return (
     <div className="App">
       <div className="blurb">
-        created by <a href="https://brokensandals.net">brokensandals</a> | source code on <a href="https://github.com/brokensandals/js-syntaxtree-explorer">github</a>
+        created by <a href="https://brokensandals.net">brokensandals</a> | source code on <a href="https://github.com/brokensandals/blunt-instrument">github</a>
       </div>
     </div>
   );
