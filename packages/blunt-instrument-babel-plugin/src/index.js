b3464723d65d0d02d7370723a2bcb1e32f20e0ce
Jacob Williams <jacobaw@gmail.com>
2019-12-25 10:58:00 -0800

    some babel plugin fixes

--- packages/blunt-instrument-babel-plugin/src/index.js
+++ packages/blunt-instrument-babel-plugin/src/index.js
@@ -71,10 +71,44 @@ function addExpressionTrace(path, { traceFnId }) {
   path.replaceWith(trace);
 }
 
+const buildPostfixRewrite = template(`
+  (() => {
+    const %%tempId%% = %%lval%%;
+    %%assignment%%;
+    return %%tempId%%;
+  })()
+`);
+
 const instrumentVisitor = {
+  UpdateExpression(path) {
+    // TODO this is super hacky
+    // Without this code, we'd be rewriting things like x++ into
+    // (() => ...)()++, which is an error.
+    const op = path.node.operator[0] + '=';
+    if (path.node.prefix) {
+      // Change ++x to x += 1
+      const replacement = types.assignmentExpression(
+        op, path.node.argument, types.numericLiteral(1));
+      replacement.nodeId = path.node.nodeId;
+      path.replaceWith(replacement);
+    } else {
+      // Change x++ to (() => { const _postfix = x; x += 1; return _postfix})
+      // TODO I feel like this code should lead to double-tracing of the lval.
+      // I don't know if I'm too tired to understand why it doesn't, or too tired
+      // to see that it really does
+      const lval = path.node.argument;
+      const tempId = path.scope.generateUidIdentifier('postfix');
+      const assignment = types.assignmentExpression(
+        op, lval, types.numericLiteral(1));
+      assignment.nodeId = path.node.nodeId;
+      const replacement = buildPostfixRewrite({ tempId, assignment, lval })
+      path.replaceWith(replacement);
+    }
+  },
+
   Expression: {
     exit(path) {
-      if (!(path.node.nodeId && path.isExpression())) return;
+      if (!(path.node.nodeId && path.isReferenced())) return;
       addExpressionTrace(path, this.state);
     }
   }
--- packages/blunt-instrument-babel-plugin/src/index.test.js
+++ packages/blunt-instrument-babel-plugin/src/index.test.js
@@ -5,13 +5,39 @@ const SOURCE_FAC = `function fac(n) {
   return n == 1 ? 1 : n * fac(n - 1);
 }`;
 
+function transform(source) {
+  return babel.transformSync(source, { plugins: [bluntInstrumentPlugin] });
+}
+
+function run(code) {
+  (0, eval(code));
+}
+
 test('testing', () => {
   const { code, ...rest } = babel.transform(SOURCE_FAC, { plugins: [bluntInstrumentPlugin] });
-  console.log(code);
-  eval(code + '; console.log(fac(7)); console.log(biEvents);');
+  run(code);
 });
 
 test('tmp', () => {
   const { code } = babel.transform(`x && y`, { plugins: [bluntInstrumentPlugin ]});
-  console.log(code);
+});
+
+test('postfix ++ operator', () => {
+  const { code } = transform('let x = 1; x++');
+  run(code);
+});
+
+test('prefix ++ operator', () => {
+  const { code } = transform('let x = 1; ++x');
+  run(code);
+});
+
+test('+= operator', () => {
+  const { code } = transform('let x = 1; x += 1');
+  run(code);
+});
+
+test('assign to MemberExpression', () => {
+  const { code } = transform('const a = [null]; a[0] = 1;');
+  run(code);
 });
