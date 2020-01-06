064fc61bb36af58092033a49771ded868157bc68
Jacob Williams <jacobaw@gmail.com>
2020-01-05 16:16:43 -0800

    start filling in babel plugin tests

--- packages/blunt-instrument-babel-plugin/src/index.test.js
+++ packages/blunt-instrument-babel-plugin/src/index.test.js
@@ -5,13 +5,33 @@ const SOURCE_FAC = `function fac(n) {
   return n == 1 ? 1 : n * fac(n - 1);
 }`;
 
-function transform(source) {
-  return babel.transformSync(source, { plugins: [bluntInstrumentPlugin] });
+function transform(source, pluginOpts = {}, babelOpts = {}) {
+  return babel.transformSync(source, { plugins: [[bluntInstrumentPlugin, pluginOpts]] , ...babelOpts });
 }
 
 function run(code) {
-  (0, eval(code));
+  return (0, eval(code));
 }
+
+describe('instrumentation object output', () => {
+  describe('assignTo', () => {
+    it('assigns to a specified variable', () => {
+      const { code } = transform('const x = 1', { outputs: { assignTo: 'result' }});
+      const wrapped = '(function(){let result;(function(){' + code + '})();return result;})()';
+      const result = run(wrapped);
+      expect(result).toHaveProperty('ast');
+      expect(result).toHaveProperty('events');
+    });
+
+    it('assigns to an object member', () => {
+      const { code } = transform('const x = 1', { outputs: { assignTo: 'this.result' }});
+      const wrapped = '(function(){let obj = {};obj.f = function(){' + code + '};obj.f();return obj.result;})()';
+      const result = run(wrapped);
+      expect(result).toHaveProperty('ast');
+      expect(result).toHaveProperty('events');
+    });
+  });
+});
 
 test('testing', () => {
   const { code, ...rest } = babel.transform(SOURCE_FAC, { plugins: [bluntInstrumentPlugin] });
@@ -22,22 +42,26 @@ test('tmp', () => {
   const { code } = babel.transform(`x && y`, { plugins: [bluntInstrumentPlugin ]});
 });
 
-test('postfix ++ operator', () => {
-  const { code } = transform('let x = 1; x++');
-  run(code);
-});
+describe('special case syntax handling', () => {
+  test('assign to MemberExpression', () => {
+    const { code } = transform('const a = [null]; a[0] = 1;');
+    run(code);
+  });
 
-test('prefix ++ operator', () => {
-  const { code } = transform('let x = 1; ++x');
-  run(code);
-});
-
-test('+= operator', () => {
-  const { code } = transform('let x = 1; x += 1');
-  run(code);
-});
-
-test('assign to MemberExpression', () => {
-  const { code } = transform('const a = [null]; a[0] = 1;');
-  run(code);
+  describe('UpdateExpression handling', () => {
+    test('postfix ++ operator', () => {
+      const { code } = transform('let x = 1; x++');
+      run(code);
+    });
+    
+    test('prefix ++ operator', () => {
+      const { code } = transform('let x = 1; ++x');
+      run(code);
+    });
+    
+    test('+= operator', () => {
+      const { code } = transform('let x = 1; x += 1');
+      run(code);
+    });
+  });
 });
