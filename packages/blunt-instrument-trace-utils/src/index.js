057ee5dc7681e01615797efdf6ed086d65afd8c7
Jacob Williams <jacobaw@gmail.com>
2020-01-07 14:26:15 -0800

    some cleanup of TraceQuerier

--- packages/blunt-instrument-trace-utils/src/index.js
+++ packages/blunt-instrument-trace-utils/src/index.js
@@ -1,11 +1,10 @@
 import * as types from '@babel/types';
 
 /**
- * Assists in querying a trace produced by blunt-instrument.
+ * Assists in querying for trace events ("trevs") in a trace produced by blunt-instrument.
  */
 export class TraceQuerier {
   /**
-   * 
    * @param {ASTQuerier} astQuerier - an ASTQuerier for the AST of the original code that blunt-instrument instrumented
    * @param {[object]} trace - the trace produced by blunt-instrument
    */
@@ -14,11 +13,37 @@ export class TraceQuerier {
     this.trace = trace;
   }
 
-  query({ filters = {} } = {}) {
+  /**
+   * Returns all trevs (trace events) matching the given criteria.
+   * 
+   * The returned trevs have the following structure:
+   * id: number - unique identifier for the trev
+   * node: Node - babel AST node corresponding to the code being executed/evaluated
+   * type: string - the type of trev, currently only "expr"
+   * value: * - for expr trevs, the result of the expression evaluation, as recorded by the configured transcriber
+   * 
+   * @param {object} criteria
+   * @param {object} criteria.filters
+   * @param {object} criteria.filters.onlyNodeIds -
+   *   keys are node IDs and values are booleans. If there is at least one
+   *   truthy value, only trevs whose node IDs match one of the keys with truthy values
+   *   will be included in the results.
+   * @param {object} criteria.filters.excludeNodeTypes -
+   *   keys are babel node types and values are booleans. Any trevs whose node type matches
+   *   one of the keys with truthy values will be excluded from the results.
+   * @returns {[object]} matching trace events in the order they were recorded
+   */
+  query(
+    {
+      filters: {
+        onlyNodeIds = {},
+        excludeNodeTypes = {}
+      }
+    } = {}) {
     const results = [];
 
-    const onlySpecificNodes = filters.onlyNodeIds &&
-      Object.values(filters.onlyNodeIds).some(Boolean);
+    const onlySpecificNodes = onlyNodeIds &&
+      Object.values(onlyNodeIds).some(Boolean);
 
     eachTrev:
     for (const trev of this.trace) {
@@ -27,15 +52,14 @@ export class TraceQuerier {
         throw new Error('Cannot find node for ID: ' + trev.nodeId);
       }
 
-      if (filters.excludeNodeTypes) {
-        for (const type of Object.keys(filters.excludeNodeTypes)) {
-          if (filters.excludeNodeTypes[type] && types.is(type, node)) {
-            continue eachTrev;
-          }
+      for (const type of Object.keys(excludeNodeTypes)) {
+        if (excludeNodeTypes[type] && types.is(type, node)) {
+          continue eachTrev;
         }
       }
+
       if (onlySpecificNodes) {
-        if (!filters.onlyNodeIds[trev.nodeId]) {
+        if (!onlyNodeIds[trev.nodeId]) {
           continue eachTrev;
         }
       }
