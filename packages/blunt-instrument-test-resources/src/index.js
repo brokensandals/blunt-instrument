52c705f8747ca52b69a7bdbf71656dea4eeba376
Jacob Williams <jacobaw@gmail.com>
2020-01-12 17:30:19 -0800

    style fix; simplify insertionSort

--- packages/blunt-instrument-test-resources/src/index.js
+++ packages/blunt-instrument-test-resources/src/index.js
@@ -6,21 +6,18 @@ examples.factorial = `function fac(n) {
 const result = fac(3);`;
 
 examples.insertionSort = `function insertionSort(array) {
-  let shifts = 0;
-
   for (let i = 0; i < array.length; i++) {
     for (let j = i - 1; j >= 0; j--) {
       if (array[j] <= array[j + 1]) {
         break;
       }
 
-      shifts++;
       const tmp = array[j + 1];
       array[j + 1] = array[j];
       array[j] = tmp;
     }
   }
-
-  return shifts;
 }
-const result = insertionSort([3, 1, 2, 5, 4]);`
+
+const a = [3, 1, 2, 5, 4];
+insertionSort(a);`
