5bbacdd319ac32df5960a6a75b822bf0e9efdfe0
Jacob Williams <jacobaw@gmail.com>
2020-01-18 14:15:07 -0800

    lint

--- packages/blunt-instrument-trace-utils/src/index.js
+++ packages/blunt-instrument-trace-utils/src/index.js
@@ -2,26 +2,26 @@ import * as types from '@babel/types';
 
 /**
  * A trace event. Currently supported types:
- * 
+ *
  * - expr: the result of evaluating an expression. The data is the result of the expression.
  * - fn-start: recorded at the beginning of a function. The data is an object containing
  *     all the named parameters of the function, plus "this" and "arguments".
  * - fn-ret: recorded when returning from a function. The data is the return value.
- * 
+ *
  * @typedef {Object} Trev
  * @property {number} [parentId] - id of the trev enclosing this one
  * @property {number} id
- * @property {("expr"|"fn-start"|"fn-ret")} type - indicates what sort of trace event this represents
+ * @property {("expr"|"fn-start"|"fn-ret")} type - what sort of trace event this represents
  * @property {string} nodeId - indicates the AST node the event is associated with
  * @property {*} [data] - data associated; the meaning varies by trev type
  */
 
- /**
+/**
   * @typedef {Trev} TrevExtended
   * @property {Object} extra
   * @property {Node} extra.node - the babel AST node representing the part of the original code
   *   that led to this trev
-  * @property {number[]} extra.ancestorIds - the trev IDs of this trev's parent, and its parent, etc.
+  * @property {number[]} extra.ancestorIds - the trev IDs of this trev's parent and its parent, etc.
   */
 
 /**
@@ -29,61 +29,65 @@ import * as types from '@babel/types';
  * Output format is an object where the keys represent the values to be whitelisted
  * or blacklisted, and the value is "true" if the key should be included in the whitelist
  * or blacklist. If the value is "false", the key should be ignored.
- * 
+ *
  * The input can already be in the output format, or can be an array or primitive.
- * 
+ *
  * Examples:
  * 'a' -> {'a': true}
  * ['a', 'b'] -> {'a': true, 'b': true}
  * {'a': true, 'b': false} -> {'a': true, 'b': false}
- * @param {*} filter 
+ * @param {*} filter
  * @returns {Object} the converted filter
  */
 function toFilterObject(filter) {
   if (filter === null || filter === undefined) {
     return {};
-  } else if (Array.isArray(filter)) {
-    const result = {};
-    for (const item of filter) {
-      result[item] = true;
-    }
-    return result;
-  } else if (typeof filter === 'object') {
-    return filter;
-  } else {
-    return {[filter]: true};
   }
+
+  if (Array.isArray(filter)) {
+    const result = {};
+    filter.forEach((item) => {
+      result[item] = true;
+    });
+    return result;
+  }
+
+  if (typeof filter === 'object') {
+    return filter;
+  }
+
+  return { [filter]: true };
 }
 
 /**
  * Assists in querying for trace events ("trevs") in a trace produced by blunt-instrument.
  */
-export class TraceQuerier {
+export class TraceQuerier { // eslint-disable-line import/prefer-default-export
   /**
-   * @param {ASTQuerier} astQuerier - an ASTQuerier for the AST of the original code that blunt-instrument instrumented
+   * @param {ASTQuerier} astQuerier - an ASTQuerier for the AST of the original uninstrumented code
    * @param {Trev[]} trace - the trace produced by blunt-instrument
    */
   constructor(astQuerier, trace) {
     this.astQuerier = astQuerier;
 
     const trevs = [];
-    for (let i = 0; i < trace.length; i++) {
+    for (let i = 0; i < trace.length; i += 1) {
       if (trace[i].id !== i + 1) {
         throw new Error(`Non-sequential trev ID ${trace[i].id}`);
       }
 
-      const parentId = trace[i].parentId;
+      const { parentId } = trace[i];
       if (parentId && parentId >= trace[i].id) {
         throw new Error(`Trev ${trace[i].id} is descended from a later parent ${parentId}`);
       }
       const ancestorIds = parentId ? [parentId].concat(trevs[parentId - 1].ancestorIds) : [];
-      
+
       const node = astQuerier.getNodeById(trace[i].nodeId);
       if (!node) {
         throw new Error(`Trev ID ${trace[i].id} has unknown node ID ${trace[i].nodeId}`);
       }
 
-      const extra = { ancestorIds, node, };
+      const extra = { ancestorIds, node };
 
       trevs.push({ extra, ...trace[i] });
     }
@@ -105,15 +109,15 @@ export class TraceQuerier {
 
   /**
    * Returns all trevs (trace events) matching the given criteria.
-   * 
+   *
    * All filters of type `object` support three different syntaxes. The following are equivalent:
    * { filters: { onlyNodeIds: { node1: true, node2: true } } }
    * { filters: { onlyNodeIds: ['node1', 'node2' ] } }
    * If you only need to supply one value for the filter, this works too:
    * { filters: { onlyNodeIds: 'node1' }}
-   * 
+   *
    * When using the first syntax, any properties where the value is `false` are disregarded.
-   * 
+   *
    * @param {object} criteria
    * @param {object} criteria.filters
    * @param {object} criteria.filters.onlyNodeIds -
@@ -125,36 +129,35 @@ export class TraceQuerier {
    *   one of the keys with truthy values will be excluded from the results.
    * @returns {TrevExtended[]} matching trace events in the order they were recorded
    */
-  query(
-    {
-      filters: {
-        onlyNodeIds = {},
-        excludeNodeTypes = {}
-      } = {}
-    } = {}) {
+  query({
+    filters: {
+      onlyNodeIds = {},
+      excludeNodeTypes = {},
+    } = {},
+  } = {}) {
     const results = [];
-    onlyNodeIds = toFilterObject(onlyNodeIds);
-    excludeNodeTypes = toFilterObject(excludeNodeTypes);
+    onlyNodeIds = toFilterObject(onlyNodeIds); // eslint-disable-line no-param-reassign
+    excludeNodeTypes = toFilterObject(excludeNodeTypes); // eslint-disable-line no-param-reassign
 
-    const onlySpecificNodes = onlyNodeIds &&
-      Object.values(onlyNodeIds).some(Boolean);
+    const onlySpecificNodes = onlyNodeIds
+      && Object.values(onlyNodeIds).some(Boolean);
 
-    eachTrev:
-    for (const trev of this.trevs) {
-      for (const type of Object.keys(excludeNodeTypes)) {
+    this.trevs.forEach((trev) => {
+      let skip = false;
+      Object.keys(excludeNodeTypes).forEach((type) => {
         if (excludeNodeTypes[type] && types.is(type, trev.extra.node)) {
-          continue eachTrev;
+          skip = true;
         }
+      });
+
+      if (!skip && onlySpecificNodes && !onlyNodeIds[trev.nodeId]) {
+        skip = true;
       }
 
-      if (onlySpecificNodes) {
-        if (!onlyNodeIds[trev.nodeId]) {
-          continue eachTrev;
-        }
+      if (!skip) {
+        results.push(trev);
       }
-
-      results.push(trev);
-    }
+    });
 
     return results;
   }
