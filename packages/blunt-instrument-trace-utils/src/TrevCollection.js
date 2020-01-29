382f573f333cfbc98950924f84c9bbe7d587288f
Jacob Williams <jacobaw@gmail.com>
2020-01-29 14:52:29 -0800

    start replacing TraceQuerier

---
+++ packages/blunt-instrument-trace-utils/src/TrevCollection.js
@@ -0,0 +1,81 @@
+export default class TrevCollection {
+  constructor(trevs, astb) {
+    this.trevs = trevs;
+    this.astb = astb;
+
+    const trevsById = new Map();
+    trevs.forEach((trev) => {
+      trevsById.set(trev.id, trev);
+    });
+    this.trevsById = trevsById;
+  }
+
+  getTrev(id) {
+    return this.trevsById.get(id);
+  }
+
+  withDenormalizedInfo() {
+    const newTrevs = [];
+    const ancestorsForParent = [];
+    const getAncestors = (parentId) => {
+      if (!parentId) {
+        return [];
+      }
+
+      const existing = ancestorsForParent[parentId];
+      if (existing) {
+        return existing;
+      }
+
+      const parent = this.getTrev(parentId);
+      if (!parent) {
+        throw new Error(`Trev has invalid parentId [${parentId}]`);
+      }
+
+      const ancestors = [...getAncestors(parent.parentId), parentId];
+      ancestorsForParent[parentId] = ancestors;
+      return ancestors;
+    };
+
+    this.trevs.forEach((trev) => {
+      const node = this.astb.getNode(trev.astId, trev.nodeId);
+      if (!node) {
+        throw new Error(`Trev id [${trev.id}] has unknown node id [${trev.nodeId}] for AST id [${trev.astId}]`);
+      }
+
+      const denormalized = {
+        ancestorIds: getAncestors(trev.parentId),
+        node,
+      };
+      newTrevs.push({ ...trev, denormalized });
+    });
+
+    return new TrevCollection(newTrevs, this.astb);
+  }
+
+  filter(fn) {
+    const filtered = this.trevs.filter(fn);
+    return new TrevCollection(filtered, this.astb);
+  }
+
+  getFacets() {
+    const facets = {
+      nodes: new Map(),
+      nodeTypes: new Map(),
+      types: new Map(),
+    };
+
+    this.trevs.forEach((trev) => {
+      const node = this.astb.getNode(trev.astId, trev.nodeId);
+      if (!node) {
+        return;
+      }
+
+      facets.nodes.set(node, (facets.nodes.get(node) || 0) + 1);
+      facets.nodeTypes.set(node.type, (facets.nodeTypes.get(node.type) || 0) + 1);
+      facets.types.set(trev.type, (facets.types.get(trev.type) || 0) + 1);
+    });
+
+    return facets;
+  }
+}
---
+++ packages/blunt-instrument-trace-utils/src/TrevCollection.test.js
@@ -0,0 +1,159 @@
+import {
+  addNodeIdsToAST,
+  ASTBundle,
+} from 'blunt-instrument-ast-utils';
+import { parseSync } from '@babel/core'; // eslint-disable-line import/no-extraneous-dependencies
+import TrevCollection from './TrevCollection';
+
+describe('TrevCollection', () => {
+  test('getTrev', () => {
+    const trevs = [{ id: 5, data: 'a' }, { id: 1, data: 'b' }];
+    const tc = new TrevCollection(trevs);
+    expect(tc.trevs).toBe(trevs);
+    expect(tc.getTrev(1)).toBe(trevs[1]);
+    expect(tc.getTrev(5)).toBe(trevs[0]);
+  });
+
+  test('filter', () => {
+    const trevs = [{ id: 5, data: 'a' }, { id: 1, data: 'b' }];
+    const astb = new ASTBundle({});
+    const tc = new TrevCollection(trevs, astb);
+    const tc2 = tc.filter((trev) => trev.data === 'a');
+    expect(tc2.trevs).toEqual([trevs[0]]);
+    expect(tc2.astb).toBe(astb);
+  });
+
+  test('getFacets', () => {
+    const ast = parseSync('x = 10; y = 12');
+    addNodeIdsToAST(ast);
+    const astb = new ASTBundle({ one: ast });
+    const node1 = ast.program.body[0].expression;
+    const node2 = ast.program.body[0].expression.right;
+    const node3 = ast.program.body[1].expression.right;
+    const trevs = [
+      {
+        id: 1,
+        astId: 'one',
+        nodeId: node1.biId,
+        type: 'expr',
+      },
+      {
+        id: 2,
+        astId: 'bogus',
+        nodeId: node1.biId,
+        type: 'expr',
+      },
+      {
+        id: 3,
+        astId: 'one',
+        nodeId: node2.biId,
+        type: 'expr',
+      },
+      {
+        id: 4,
+        astId: 'one',
+        nodeId: node1.biId,
+        type: 'expr',
+      },
+      {
+        id: 5,
+        astId: 'one',
+        nodeId: node3.biId,
+        type: 'fn-start', // not valid for this node type, but fine for testing
+      },
+    ];
+    const facets = new TrevCollection(trevs, astb).getFacets();
+    expect(facets.nodes.get(node1)).toEqual(2);
+    expect(facets.nodes.get(node2)).toEqual(1);
+    expect(facets.nodes.get(node3)).toEqual(1);
+    expect(facets.nodeTypes.get('AssignmentExpression')).toEqual(2);
+    expect(facets.nodeTypes.get('NumericLiteral')).toEqual(2);
+    expect(facets.types.get('expr')).toEqual(3);
+    expect(facets.types.get('fn-start')).toEqual(1);
+  });
+
+  describe('withDenormalizedInfo', () => {
+    let ast;
+    let astb;
+
+    beforeEach(() => {
+      ast = parseSync(`
+        function pow(x, n) {
+          return n === 1 ? x : x * pow(x, n - 1);
+        }
+        pow(2, 3);
+      `);
+      addNodeIdsToAST(ast);
+      astb = new ASTBundle({ test: ast });
+    });
+
+    it('throws error for invalid parentId', () => {
+      const trevs = [{
+        id: 2,
+        parentId: 1,
+        astId: 'test',
+        nodeId: 1,
+      }];
+      const tc = new TrevCollection(trevs, astb);
+      expect(() => tc.withDenormalizedInfo()).toThrowError('Trev has invalid parentId [1]');
+    });
+
+    it('throws error for invalid astId', () => {
+      const trevs = [{
+        id: 2,
+        astId: 'bogus',
+        nodeId: 1,
+      }];
+      const tc = new TrevCollection(trevs, astb);
+      expect(() => tc.withDenormalizedInfo()).toThrowError('Trev id [2] has unknown node id [1] for AST id [bogus]');
+    });
+
+    it('throws error for invalid nodeId', () => {
+      const trevs = [{
+        id: 2,
+        astId: 'test',
+        nodeId: 1000,
+      }];
+      const tc = new TrevCollection(trevs, astb);
+      expect(() => tc.withDenormalizedInfo()).toThrowError('Trev id [2] has unknown node id [1000] for AST id [test]');
+    });
+
+    it('populates `node`', () => {
+      const trevs = [{
+        id: 2,
+        astId: 'test',
+        nodeId: 3,
+      }];
+      const tc = new TrevCollection(trevs, astb).withDenormalizedInfo();
+      expect(tc.trevs[0]).not.toBe(trevs[0]);
+      expect(tc.trevs[0].denormalized.node).toBe(ast.program.body[0]);
+    });
+
+    it('populates `ancestorIds`', () => {
+      const trevs = [
+        {
+          id: 2,
+          parentId: 1,
+          astId: 'test',
+          nodeId: 5,
+        },
+        {
+          id: 3,
+          astId: 'test',
+          nodeId: 3,
+        },
+        {
+          id: 1,
+          parentId: 3,
+          astId: 'test',
+          nodeId: 3,
+        },
+      ];
+      const tc = new TrevCollection(trevs, astb).withDenormalizedInfo();
+      expect(tc.trevs[0]).not.toBe(trevs[0]);
+      expect(tc.trevs[0].denormalized.ancestorIds).toEqual([3, 1]);
+      expect(tc.trevs[1].denormalized.ancestorIds).toEqual([]);
+      expect(tc.trevs[2].denormalized.ancestorIds).toEqual([3]);
+    });
+  });
+});
