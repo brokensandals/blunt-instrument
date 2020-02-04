b56bc017faafd25b838e2ec0721432ed6cfbc83a
Jacob Williams <jacobaw@gmail.com>
2020-02-04 01:12:12 -0800

    some docs on ArrayTrace

--- packages/blunt-instrument-trace-utils/src/ArrayTrace.js
+++ packages/blunt-instrument-trace-utils/src/ArrayTrace.js
@@ -1,5 +1,20 @@
 import { Encoder } from 'object-graph-as-json';
 
+/**
+ * Holds an in-memory trace of some instrumented code's execution.
+ * This should be attached to a Tracer using the attach() method. Subsequently, any trevs
+ * or AST registrations reported by the Tracer will be stored.
+ *
+ * Trevs are stored in an array in a property named `trevs`. The `data` field of each trev
+ * is encoded using object-graph-as-json so that later mutations to the object will not
+ * alter the trev.
+ *
+ * ASTs are stored in an object in a property named `asts`. The key is the AST id, and the
+ * value is the root babel node of the AST.
+ *
+ * You can set the property `onChange` to a function, and it will be called after any additions
+ * to the `asts` object or the `trevs` array.
+ */
 export default class ArrayTrace {
   constructor({ encoder = new Encoder() } = {}) {
     this.asts = {};
@@ -7,6 +22,10 @@ export default class ArrayTrace {
     this.trevs = [];
   }
 
+  /**
+   * Configures the given Tracer to send events to this ArrayTrace instance.
+   * @param {Tracer} tracer
+   */
   attach(tracer) {
     // eslint-disable-next-line no-param-reassign
     tracer.onRegisterAST = this.handleRegisterAST.bind(this);
