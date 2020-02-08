1be0d069e4b3419a6d8fbb9ab0627925b2f74b32
Jacob Williams <jacobaw@gmail.com>
2020-02-08 12:50:56 -0800

    simplify AnnotatedCodeView

--- packages/blunt-instrument-ui/src/components/AnnotatedCode/AnnotatedCodeView.js
+++ packages/blunt-instrument-ui/src/components/AnnotatedCode/AnnotatedCodeView.js
@@ -22,78 +22,6 @@ function findChildNodes(node) {
   return Object.keys(node).flatMap(k => findNodes(node[k]));
 }
 
-function CodeForNode({
-  code,
-  highlightedNodeKey,
-  node,
-  onHoveredNodeChange,
-  onNodeSelectedToggle,
-  selectedNodeKeys,
-}) {
-  const { biKey: nodeKey, start, end } = node;
-  let cur = start;
-  const elements = [];
-
-  // For code using shorthand object property notation, i.e. `{x}` instead of `{x: x}`,
-  // there are three nodes corresponding to the code `x`: the ObjectProperty, ObjectProperty.id,
-  // and ObjectProperty.value. This special case is here to avoid displaying `x` multiple times,
-  // and to make sure the one we display is the one that trevs will actually be attached to.
-  if (node.type === 'ObjectProperty' && node.shorthand) {
-    return <CodeForNode key={node.value.biKey}
-                        highlightedNodeKey={highlightedNodeKey}
-                        node={node.value}
-                        selectedNodeKeys={selectedNodeKeys}
-                        code={code}
-                        onHoveredNodeChange={onHoveredNodeChange}
-                        onNodeSelectedToggle={onNodeSelectedToggle} />;
-  }
-
-  // TODO does this sorting need to look at 'end' too?
-  const children = findChildNodes(node).sort((a, b) => a.start - b.start);
-  for (const child of children) {
-    if (cur < child.start) {
-      elements.push(code.slice(cur, child.start));
-    }
-    
-    elements.push(
-      <CodeForNode key={child.biKey}
-                     highlightedNodeKey={highlightedNodeKey}
-                     node={child}
-                     selectedNodeKeys={selectedNodeKeys}
-                     code={code}
-                     onHoveredNodeChange={onHoveredNodeChange}
-                     onNodeSelectedToggle={onNodeSelectedToggle} />
-    );
-
-    cur = child.end;
-  }
-
-  if (cur < end) {
-    elements.push(code.slice(cur, end));
-  }
-
-  const handleMouseOver = nodeKey && onHoveredNodeChange ? (event) => {
-    onHoveredNodeChange(nodeKey);
-    event.stopPropagation();
-  } : null;
-
-  const handleClick = nodeKey && onNodeSelectedToggle ? (event) => {
-    onNodeSelectedToggle(nodeKey);
-    event.stopPropagation();
-  } : null;
-
-  const className = [
-    nodeKey && nodeKey === highlightedNodeKey ? 'highlighted' : '',
-    nodeKey && selectedNodeKeys.includes(nodeKey) ? 'selected' : '',
-  ].join(' ')
-
-  return <span className={className}
-               onClick={handleClick}
-               onMouseOver={handleMouseOver}>
-    {elements}
-  </span>
-}
-
 function AnnotatedCodeView({
   ast,
   highlightedNodeKey,
@@ -101,16 +29,66 @@ function AnnotatedCodeView({
   onNodeSelectedToggle = null,
   selectedNodeKeys,
 }) {
+  const code = ast.codeSlice;
+
+  function CodeForNode({ node }) {
+    const { biKey: nodeKey, start, end } = node;
+    let cur = start;
+    const elements = [];
+  
+    // For code using shorthand object property notation, i.e. `{x}` instead of `{x: x}`,
+    // there are three nodes corresponding to the code `x`: the ObjectProperty, ObjectProperty.id,
+    // and ObjectProperty.value. This special case is here to avoid displaying `x` multiple times,
+    // and to make sure the one we display is the one that trevs will actually be attached to.
+    if (node.type === 'ObjectProperty' && node.shorthand) {
+      return <CodeForNode key={node.value.biKey} node={node.value} />;
+    }
+  
+    // TODO does this sorting need to look at 'end' too?
+    const children = findChildNodes(node).sort((a, b) => a.start - b.start);
+    for (const child of children) {
+      if (cur < child.start) {
+        elements.push(code.slice(cur, child.start));
+      }
+      
+      elements.push(
+        <CodeForNode key={child.biKey} node={child} />
+      );
+  
+      cur = child.end;
+    }
+  
+    if (cur < end) {
+      elements.push(code.slice(cur, end));
+    }
+  
+    const handleMouseOver = nodeKey && onHoveredNodeChange ? (event) => {
+      onHoveredNodeChange(nodeKey);
+      event.stopPropagation();
+    } : null;
+  
+    const handleClick = nodeKey && onNodeSelectedToggle ? (event) => {
+      onNodeSelectedToggle(nodeKey);
+      event.stopPropagation();
+    } : null;
+  
+    const className = [
+      nodeKey && nodeKey === highlightedNodeKey ? 'highlighted' : '',
+      nodeKey && selectedNodeKeys.includes(nodeKey) ? 'selected' : '',
+    ].join(' ')
+  
+    return <span className={className}
+                 onClick={handleClick}
+                 onMouseOver={handleMouseOver}>
+      {elements}
+    </span>
+  }
+
   const clearHover = onHoveredNodeChange ? () => onHoveredNodeChange(null) : null;
   return (
     <pre className="AnnotatedCode" onMouseLeave={clearHover}>
       <code>
-        <CodeForNode highlightedNodeKey={highlightedNodeKey}
-                       node={ast}
-                       selectedNodeKeys={selectedNodeKeys}
-                       code={ast.codeSlice}
-                       onHoveredNodeChange={onHoveredNodeChange}
-                       onNodeSelectedToggle={onNodeSelectedToggle} />
+        <CodeForNode node={ast} />
       </code>
     </pre>
   );
