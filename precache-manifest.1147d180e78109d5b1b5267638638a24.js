27f0ba9da2b1e9f20426393d514c3e69cd356e82
Jacob Williams <jacobaw@gmail.com>
2020-02-08 19:58:15 -0800

    initial commit of UI and example dist

---
+++ precache-manifest.1147d180e78109d5b1b5267638638a24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0da511d779d105ab783fdd636fab86",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76d1a34ebfd201071baa",
+    "url": "./static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "27e744d7dcc88676c2f0",
+    "url": "./static/css/main.cba06675.chunk.css"
+  },
+  {
+    "revision": "76d1a34ebfd201071baa",
+    "url": "./static/js/2.7fe35942.chunk.js"
+  },
+  {
+    "revision": "c13ff04e4e8bee1dd738e580a7b59bfb",
+    "url": "./static/js/2.7fe35942.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27e744d7dcc88676c2f0",
+    "url": "./static/js/main.f8795ce5.chunk.js"
+  },
+  {
+    "revision": "6dc0fb6514c310d6b823",
+    "url": "./static/js/runtime-main.eea250d7.js"
+  }
+]);
